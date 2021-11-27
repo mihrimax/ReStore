8bea9ffd2dcbb1080a0b94c863e208b9c5818ca8
mihrimax <mihriban.ozsoy34@gmail.com>
2021-11-28 00:18:22 +0300

    added lists api functions

---
+++ API/Entities/Product.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }        
+        //Sqlite doesn't understand decimal
+        public long Price { get; set; }
+        public string PictureUrl { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using API.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            //for automatically dispose instead of scope.dispose in finally scope, any resource that we're using as part of this.
+            using var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                context.Database.Migrate();
+                DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Problem migrating data");
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
